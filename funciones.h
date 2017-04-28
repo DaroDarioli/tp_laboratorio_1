23bdad70866dfd9397ceba0b3382bc45880fcf5b
DaroDarioli <darioesteban21@hotmail.com>
2017-04-27 21:34:42 -0300

    borrado
    borrado

--- funciones.h
+++
@@ -1,9 +0,0 @@
-#ifndef FUNCIONES_H_INCLUDED
-#define FUNCIONES_H_INCLUDED
-#endif // FUNCIONES_H_INCLUDED
-/*Laboratorio - TP 1 - Olinuck Dario */
-float suma(float a, float b);
-float resta(float a, float b);
-float division(float a, float b);
-float multiplicacion(float a, float b);
-float factorial(float a);
